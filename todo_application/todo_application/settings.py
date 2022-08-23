02bb26f329b1d45253a219f654e717505c778b8b
Pakeeza Ehsan <pakeeza22pk@gmail.com>
2022-08-23 16:23:29 +0500

    Update settings.py

--- todo_application/todo_application/settings.py
+++ todo_application/todo_application/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yvt9#kd#v0j0i@4atgnq=^r025zz40yqc90f90vq8k1%)yszt-'
+SECRET_KEY = 'django-insecure-##################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': 'blog_data',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'pakiza',
+        'USER': 'user',
         'PASSWORD': 'password'}
 }
 # Password validation
